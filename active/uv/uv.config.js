e6bfc87162edf2027ea649f39760f2f105d7aa21
Leah Conor 3 <leahconor91@gmail.com>
2026-02-13 22:45:25 -0500

    Refactor uv.config.js for concise configuration

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,12 +1,1 @@
-self.__uv$config = {
-    prefix: '/active/go/',
-    bare: '/bare/',
-
-    encodeUrl: (url) => Ultraviolet.codec.xor.encode(url),
-    decodeUrl: (url) => Ultraviolet.codec.xor.decode(url),
-
-    handler: '/active/uv/uv.handler.js',
-    bundle: '/active/uv/uv.bundle.js',
-    config: '/active/uv/uv.config.js',
-    sw: '/active/uv/uv.sw.js',
-};
+self.__uv$config = { prefix: '/active/go/', bare:'/bare/', encodeUrl: Ultraviolet.codec.xor.encode, decodeUrl: Ultraviolet.codec.xor.decode, handler: '/active/uv/uv.handler.js', bundle: '/active/uv/uv.bundle.js', config: '/active/uv/uv.config.js', sw: '/active/uv/uv.sw.js', };
