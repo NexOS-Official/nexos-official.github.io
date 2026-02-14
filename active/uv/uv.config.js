035ab74ec69126f4f07a47194aad98c156212741
Leah Conor 3 <leahconor91@gmail.com>
2026-02-13 22:50:38 -0500

    Update uv.config.js

--- active/uv/uv.config.js
+++ active/uv/uv.config.js
@@ -1,1 +1,1 @@
-self.__uv$config = { prefix: '/active/go/', bare:'/bare/', encodeUrl: Ultraviolet.codec.xor.encode, decodeUrl: Ultraviolet.codec.xor.decode, handler: '/active/uv/uv.handler.js', bundle: '/active/uv/uv.bundle.js', config: '/active/uv/uv.config.js', sw: '/active/uv/uv.sw.js', };
+self.__uv$config = { prefix: '/active/learn/', bare:'/bare/', encodeUrl: Ultraviolet.codec.xor.encode, decodeUrl: Ultraviolet.codec.xor.decode, handler: '/active/uv/uv.handler.js', bundle: '/active/uv/uv.bundle.js', config: '/active/uv/uv.config.js', sw: '/active/uv/uv.sw.js', };
