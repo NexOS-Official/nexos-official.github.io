571f4480aaea54db3b8df58fbbd2a726eff488e8
Leah Conor 3 <leahconor91@gmail.com>
2026-02-17 16:17:11 -0500

    Update uv-sw.js

--- active/uv-sw.js
+++ active/uv-sw.js
@@ -2,4 +2,18 @@ importScripts("/active/uv/uv.sw.js");
 
 const sw = new UVServiceWorker();
 
-self.addEventListener("fetch", (event) => event.respondWith(sw.fetch(event)));
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  try {
+    event.respondWith(sw.fetch(event));
+  } catch (err) {
+    event.respondWith(fetch(event.request));
+  }
+});
