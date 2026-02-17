2c19c8a6bd22a379bb7320c13c1527256238e93b
Leah Conor 3 <leahconor91@gmail.com>
2026-02-17 16:19:43 -0500

    Update search.js

--- active/search.js
+++ active/search.js
@@ -1,25 +1,32 @@
 "use strict";
-
+/**
+ *
+ * @param {string} input
+ * @param {string} template Template for a search query.
+ * @returns {string} Fully qualified URL
+ */
 function search(input, template) {
-  input = input.trim();
-
-  if (!input) {
-    return template.replace("%s", "");
+  try {
+    // input is a valid URL:
+    // eg: https://example.com, https://example.com/test?q=param
+    return new URL(input).toString();
+  } catch (err) {
+    // input was not a valid URL
   }
 
   try {
-    const url = new URL(input);
-    if (url.protocol === "http:" || url.protocol === "https:") {
-      return url.toString();
-    }
-  } catch {}
+    // input is a valid URL when http:// is added to the start:
+    // eg: example.com, https://example.com/test?q=param
+    const url = new URL(`http://${input}`);
+    // only if the hostname has a TLD/subdomain
+    if (url.hostname.includes(".")) return url.toString();
+  } catch (err) {
+    // input was not valid URL
+  }
 
-  try {
-    const url = new URL("http://" + input);
-    if (/^[a-z0-9.-]+\.[a-z]{2,}$/i.test(url.hostname)) {
-      return url.toString();
-    }
-  } catch {}
+  // input may have been a valid URL, however the hostname was invalid
 
+  // Attempts to convert the input to a fully qualified URL have failed
+  // Treat the input as a search query
   return template.replace("%s", encodeURIComponent(input));
 }
