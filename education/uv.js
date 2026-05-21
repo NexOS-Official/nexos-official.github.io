0c35fbfe592562c0b1ebb59d79415b2436110661
VΔƗŁ <159073116+1056516@users.noreply.github.com>
2026-05-20 21:59:31 -0400

    Update uv.js

--- education/uv.js
+++ education/uv.js
@@ -1,6 +1,6 @@
-importScripts('/education/school/uv.bundle.js');
-importScripts('/education/school/uv.config.js');
-importScripts('/education/school/uv.sw.js');
+importScripts('/education/uv/uv.bundle.js');
+importScripts('/education/uv/uv.config.js');
+importScripts('/education/uv/uv.sw.js');
 importScripts('https://arc.io/arc-sw-core.js');
 
 const sw = new UVServiceWorker();
